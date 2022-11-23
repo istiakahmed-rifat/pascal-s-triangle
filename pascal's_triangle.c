cb2d823055afbedcf113c9c41731c8abd3d89fe5
Istiak Ahmed Rifat <118871656+istiakahmed-rifat@users.noreply.github.com>
2022-11-23 11:44:32 +0600

    Add files via upload

--- pascal's_triangle.c
+++ pascal's_triangle.c
@@ -5,19 +5,22 @@ int factorial(int r){
     else{f= r*factorial(r-1);}
     return f;
 }
-void triangle(int n){
-for(int line=n;line>=0; line--){ //prints n+1 number of lines
-
+void triangle(int lines){
+for(int n=0;n<=lines; n++){ //prints n+1 number of lines
+  printf("n=%d ",n);
+  
+  for(int space = lines-n; space>=0;space--){printf(" ");}
     for(int r=0;r<=n;r++){
         printf("%d", (factorial(n))/(factorial(r)*factorial(n-r))); //formula for nCr
+        printf(" ");
     }
     printf("\n");
     }
 }
 int main() {
-    int n;
+    int lines;
     printf("what is n?\n");
-    scanf("%d", &n);
-    triangle(n);
+    scanf("%d", &lines);
+    triangle(lines);
     return 0;
 }
